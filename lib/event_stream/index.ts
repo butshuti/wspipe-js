c4ab6caf187a6bab2a7e99570e1f3d5c44d83bc5
Theogene <TheogeneBucuti@my.unt.edu>
2019-03-18 12:06:45 -0500

    export statusMonitor interface

--- lib/event_stream/index.ts
+++ lib/event_stream/index.ts
@@ -1,5 +1,6 @@
 export * from './module/DiscoveryClient';
 export * from './module/EventHandler';
+export * from './module/StatusMonitor';
 export * from './module/EventStream';
 export * from './module/MockEventStream';
 export * from './module/Peer';
