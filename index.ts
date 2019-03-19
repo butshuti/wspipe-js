e2aaf1239b49219ff3e690983e9ad5555e87ca96
Theogene <TheogeneBucuti@my.unt.edu>
2019-03-18 19:16:24 -0500

    Testing amd.

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './lib/DiscoveryClient';
+export * from './lib/EventHandler';
+export * from './lib/EventStream';
+export * from './lib/Peer';
+export * from './lib/RelayedWSEventStream';
+export * from './lib/WSEventStream';
+export * from './lib/WSEventStreamConfig';
